b76b80a000fa4d6d0cc057473ffac21088fe73b4
earllee <earlvlee@gmail.com>
2013-11-09 12:07:01 -0500

    instantiating sql

--- var/www/mine.php
+++ var/www/mine.php
@@ -35,7 +35,7 @@ $results;
 $list;
 
 // Connect to MySQL to store friend info
-$con = new mysqli_connect('http://54.200.89.7', 'root', 'monster', 'core');
+$con = new mysqli('http://54.200.89.7', 'root', 'monster', 'core');
 if (mysqli_connect_errno()) {
   echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
 }
