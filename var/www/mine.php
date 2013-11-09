15221344c3b01685796438cb9fa7cba47275349f
earllee <earlvlee@gmail.com>
2013-11-09 06:50:27 -0500

    getting friend photos

--- var/www/mine.php
+++ var/www/mine.php
@@ -10,10 +10,10 @@ $current = 'Access token: ';
 $current .= $access_token;
 
 $facebook = new Facebook(array(
-#  'appId'  => '755517144464680',
-#  'secret' => '60f3a2763be6b3108359ccabcda159ea',
-  'appId'  => '620801497991130',
-  'secret' => '42a1b5e473f05340246cdaa42c7cbba5',
+  'appId'  => '755517144464680',
+  'secret' => '60f3a2763be6b3108359ccabcda159ea',
+#  'appId'  => '620801497991130',
+#  'secret' => '42a1b5e473f05340246cdaa42c7cbba5',
 ));
 
 $facebook->setAccessToken($access_token);
@@ -23,8 +23,21 @@ $user = $facebook->getUser();
 $current .= '\nUser ID: ';
 $current .= $user;
 
-$result = $facebook->api('/'.$user.'/friends?fields=name,gender',array('access_token' => $access_token)); 
-echo $current;
+// Calvin UID: 1372209588
+// Shenil UID: 100001552561170
+// Earl UID: 589405438
+$result = $facebook->api('/'.$user.'/friends?fields=name',array('access_token' => $access_token)); 
+
+foreach($result['data'] as $friend) {
+  $friendID = $friend->id;
+  $albums = $facebook->api('/'.$friendID.'/albums', array('access_token' => $access_token));
+  foreach($albums['data'] as $album) {
+    if ($album['name'] == 'Profile Pictures') {
+      $prof_pics = $facebook->api('/'.$album['id'], array('fields' => 'photos.fields(picture, source)', 'access_token' => $access_token));
+      echo $prof_pics;
+    }
+  }
+}
 echo 'Albums: ';
 
 $albums = $facebook->api('/'.$user.'/albums', array('access_token' => $access_token));
